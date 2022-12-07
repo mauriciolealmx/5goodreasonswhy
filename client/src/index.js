1b262d49b1a4806387b127034795089caf25be3a
Mauricio Leal <mauriciolealmx@gmail.com>
2022-12-06 21:40:12 -0600

    chore: add react router dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import App from './App';
+import Post from './components/Post';
+import Home from './components/Home';
 
 import './config/setAmplifyConfig';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:postId" element={<Post />} />
+        </Routes>
+      </App>
+    </BrowserRouter>
   </React.StrictMode>
 );
