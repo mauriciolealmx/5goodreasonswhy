7f0405c50d6844cfdccad5e892821affe2f28537
Mauricio Leal <mauriciolealmx@gmail.com>
2022-12-06 20:32:44 -0600

    chore: list all posts from userId

--- services/functions/posts/list.js
+++ services/functions/posts/list.js
@@ -1,5 +1,24 @@
+import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb';
 import handler from '../../utils/handler';
 
+const client = new DynamoDBClient();
+
 export const main = handler(async () => {
-  return 'List of posts!';
+  // What about all posts from all users?
+  const input = {
+    TableName: process.env.TABLE_NAME,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': { S: 'random-id' },
+    },
+  };
+
+  try {
+    const command = new QueryCommand(input);
+    const results = await client.send(command);
+
+    return results.Items;
+  } catch (err) {
+    console.error(err);
+  }
 });
--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -1,7 +1,18 @@
-import { Api } from '@serverless-stack/resources';
+import { Api, use } from '@serverless-stack/resources';
+import StorageStack from './StorageStack';
 
 const ApiStack = ({ stack }) => {
+  const { table } = use(StorageStack);
+  
   const api = new Api(stack, 'Api', {
+    defaults: {
+      function: {
+        permissions: [table],
+        environment: {
+          TABLE_NAME: table.tableName,
+        },
+      },
+    },
     routes: {
       'GET /': 'functions/posts/list.main',
     },
